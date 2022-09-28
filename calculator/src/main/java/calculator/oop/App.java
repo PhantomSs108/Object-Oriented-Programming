09f882e9ab973a3bb022e4171bb49c2be17df7c6
Theanh Nguyen <85526287+PhantomSs108@users.noreply.github.com>
2022-09-28 02:20:09 -0700

    added menuprint method, prompt user for command

--- calculator/src/main/java/calculator/oop/App.java
+++ calculator/src/main/java/calculator/oop/App.java
@@ -1,13 +1,29 @@
 package calculator.oop;
 
-/**
- * Hello world!
- *
- */
+import java.util.*;
+
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        double a = 0.000;
+        double b = 0.000;
+        printMenu(a, b);
+
+    }
+
+    private static void printDashes() {
+        for (int i = 0; i < 60; i++)
+            System.out.print("-");
+        System.out.println();
+    }
+
+    private static void printMenu(double a, double b) {
+        printDashes();
+        System.out.println("Chavvi Calc");
+        printDashes();
+        System.out.printf("A = %.3f", a);
+        System.out.print("           ");
+        System.out.printf("B = %.3f", b);
     }
 }
