281417dae1be1889b13dac3cbf1cc657472cdefe
Theanh Nguyen <85526287+PhantomSs108@users.noreply.github.com>
2022-10-13 23:22:16 -0700

    created new Maven project, ran hello world

---
+++ cats/src/main/java/cats/example/App.java
@@ -0,0 +1,13 @@
+package cats.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
